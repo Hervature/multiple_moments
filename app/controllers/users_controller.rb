469da55d6ede765610b93588fd6ac67c27f61cf6
Zachary Hervieux-Moore <zachary.moore.1993@gmail.com>
2014-11-06 10:33:10 -0800

    Check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ before_action :authenticate_user!
     respond_to do |format|
       if @user.save
         format.html { redirect_to users_path, notice: 'User was successfully created.' }
-        format.json { render action: 'index', status: :created, location: @user }
+        format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
