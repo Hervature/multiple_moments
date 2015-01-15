b481f2df0e49d51a7cda1e11b5f305ac7b130eec
Zachary Hervieux-Moore <zachary.moore.1993@gmail.com>
2015-01-15 02:26:05 -0800

    Typo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require tree .
+//= require_tree .
 //= require jquery
 //= require jquery_ujs
 //= require uikit.min.js
